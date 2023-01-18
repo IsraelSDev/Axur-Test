7da791f0a7abd51a4bec2c8cf60d839f1e7cb28b
Israel S. Santos <israelss@primeit.com.br>
2023-01-18 00:34:12 -0300

    test environment variable

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './styles/App.scss';
 
+
 function App() {
+
+  console.log("Process: " + process.env.REACT_APP_API_URL)
+
   return (
     <div className="App">
-      <h1>Olá mundo</h1>
+      <h1>API URL: {process.env.REACT_APP_API_URL}</h1>
     </div>
   );
 }
